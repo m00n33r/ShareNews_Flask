f5f3a42a66d225262b01e65194d6699920be86f7
m00n33r <muyio0905@gmail.com>
2021-04-26 15:27:12 +0300

    made first home page

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
+from data.__all_models import User, News
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 db_session.global_init("db/logs.db")
+
+@app.route('/')
+@app.route('/home')
+def home():
+    session = db_session.create_session()
+    return render_template('home.html', title='ShareNews.com')
 
 
 if __name__ == '__main__':
