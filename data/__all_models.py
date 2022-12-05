e4c19cbc97445af23ed579a7ae129f55d22043a1
m00n33r <muyio0905@gmail.com>
2022-12-05 15:52:35 +0300

    Fixed Bugs

--- data/__all_models.py
+++ data/__all_models.py
@@ -33,7 +33,7 @@ class News(SqlAlchemyBase):
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
+                                     default=datetime.datetime.date)
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
